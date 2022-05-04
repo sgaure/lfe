8880f12d7199d4e3b232809143f03e5cfc78f3cb
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2022-05-04 22:34:14 +0200

    Internal devtools

--- inst/devtools_internal.R
+++ inst/devtools_internal.R
@@ -13,8 +13,14 @@ devtools::check_win_devel()
 devtools::check_win_release()
 devtools::check_win_oldrelease()
 
-devtools::check_rhub(interactive = FALSE)
-devtools::check_rhub(platform = c("solaris-x86-patched", "solaris-x86-patched-ods"),
+
+## Check on R-hub
+plats_default <- c("windows-x86_64-devel", "ubuntu-gcc-release", "fedora-clang-devel", 
+                   "linux-x86_64-rocker-gcc-san") # from: rhub:::default_cran_check_platforms(".")
+plats_add <- c("solaris-x86-patched", "solaris-x86-patched-ods")
+plats_all <-  c(plats_default, plats_add)
+devtools::check_rhub(platform = plats_all,
+                     env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false"), ##testhat not working on Solaris by May 2022
                      interactive = FALSE)
 
 ## reverse dependencies
@@ -24,3 +30,4 @@ revdepcheck::revdep_check(num_workers = 1)
 ### Upload to CRAN
 devtools::release_checks()
 devtools::release()
+# direct: devtools::submit_cran()
